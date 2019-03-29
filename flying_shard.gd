eb9220de9b8e44b4565a01296d8d0284c0b8d9f8
Tim Slippy, voxelv <slippytim@gmail.com>
2019-03-28 23:48:25 -0700

    random rotation and speed

--- flying_shard.gd
+++ flying_shard.gd
@@ -5,20 +5,28 @@ signal finished
 onready var shard_amt = find_node("shard_amt")
 onready var shard_sprite = find_node("shard_sprite")
 
-export(int) var shard_speed = 375
+export(int) var shard_speed = 500
+export(int) var shard_speed_variance = 200
 
 var shard_count = 0
 var finished = true
 var from = Vector2()
 var to = Vector2()
+var rotation_speed = 0.0
+var move_speed = 0
 
 func _ready():
-	pass
+	randomize()
+	rotation_speed = rand_range(90, 360*1.5)
+	if randi() % 2 == 0:
+		rotation_speed *= -1
+	move_speed = rand_range(shard_speed - shard_speed_variance, shard_speed + shard_speed_variance)
 
 func _process(delta):
 	if not finished:
-		var dist_to_move = shard_speed * delta
+		var dist_to_move = move_speed * delta
 		position += (to - from).normalized() * dist_to_move
+		shard_sprite.rotate(deg2rad(rotation_speed) * delta)
 		
 		if position.distance_squared_to(to) < (dist_to_move * dist_to_move):
 			position = to
